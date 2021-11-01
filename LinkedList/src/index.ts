529cd4b0309af38ad4c4f527c92f2e4489538acf
Filip <fivanusec@gmail.com>
2021-11-01 09:07:18 +0100

    Linked list

---
+++ LinkedList/src/index.ts
@@ -0,0 +1,20 @@
+import { LinkedList } from "./LinkedList/.";
+interface Post {
+  title: string;
+}
+
+const main = () => {
+  const list = new LinkedList<Post>();
+
+  console.log(list.traverse());
+
+  list.insertAtEnd({ title: "Post A" });
+  list.insertAtEnd({ title: "Post B" });
+  list.insertInBegin({ title: "Post C" });
+  list.insertInBegin({ title: "Post D" });
+
+  console.log(list.traverse());
+  console.log(list.search(({ title }) => title === "Post A"));
+};
+
+main();
